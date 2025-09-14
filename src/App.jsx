6767b75e403cbd8caec391ec1dc4b62a34f87183
Jorge Matos <jisapjisap@gmail.com>
2025-09-14 15:21:19 +0200

    Initialize car rental website with React and Vite setup, including ESLint configuration, essential components, and sample data for cars and blogs. Add .gitignore and basic styling with Tailwind CSS.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Nav from "./Components/Nav"
+
+function App() {
+
+  return (
+    <>
+      <Router>
+        <Nav />
+      </Router>
+    </>
+  )
+}
+
+export default App
