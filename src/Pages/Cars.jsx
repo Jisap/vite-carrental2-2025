6c9e327ecf529d615e9240f45980fff1f8afd296
Jorge Matos <jisapjisap@gmail.com>
2025-09-19 17:50:36 +0200

    Add banner section styles and enhance Cars page layout. Introduce a new banner with background image and overlay, and implement a responsive grid for car listings with improved visual elements and booking functionality.

--- src/Pages/Cars.jsx
+++ src/Pages/Cars.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import carddata from '../../Cars.json';
-
-
+import { Link } from 'react-router-dom';
 
 const Cars = () => {
 
@@ -139,6 +138,70 @@ const Cars = () => {
                 </li>
               ))}
             </ul>
+          </div>
+        </div>
+
+        <div className='flex-1 animate-fade-in'>
+          <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
+            {filteredCars.length > 0 ? (
+              filteredCars.map((car) => (
+                <div
+                  key={car.id}
+                  className="car-item group bg-[#1e1f22] relative w-full"
+                >
+                  <div className="car-image w-full relative h-[250px] overflow-hidden after:content-[''] after:absolute after:inset-0 after:z-0 after:bg-gradient-to-b after:from-transparent after:to-[#1E1F22]">
+                    <img
+                      src={car.image}
+                      alt={car.title}
+                      className="w-full h-full object-cover group-hover:scale-110 transition-all duration-300"
+                    />
+
+                    <div className="car-info absolute bottom-0 p-5 left-0 z-10">
+                      <h4 className="text-2xl md:text-3xl font-bricolage text-white font-semibold">
+                        {car.name}
+                      </h4>
+
+                      <span className="text-red-100 font-bricolage text-xl">
+                        {car.type}
+                      </span>
+                    </div>
+                  </div>
+
+                  <div className="car-content p-5 py-10 relative">
+                    <ul className="flex gap-3 justify-between items-center flex-wrap">
+                      <li className="text-gray-300 text-lg md:text-xl">
+                        <i className="fa-regular fa-user text-primary pe-2"></i>
+                        {car.seats} Seats
+                      </li>
+                      <li className="text-gray-300 text-lg md:text-xl">
+                        <i className="fa-regular fa-user text-primary pe-2"></i>
+                        {car.transmission}
+                      </li>
+                      <li className="text-gray-300 text-lg md:text-xl">
+                        <i className="fa-regular fa-user text-primary pe-2"></i>
+                        {car.speed}
+                      </li>
+                    </ul>
+
+                    <div className="flex justify-between items-center mt-12">
+                      <h4 className="text-2xl md:text-4xl font-bold font-bricolage text-white">
+                        $ {car.price}/day
+                      </h4>
+
+                      <Link to={`/car/${car.id}`}>
+                        <button
+                          className=" text-white bg-red-600 px-5 py-3 text-lg md:text-xl rounded-full cursor-pointer"
+                        >
+                          Book Now
+                        </button>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              ))
+            ):( 
+              <p className='text-white text-xl'>No cars found</p>
+            )}
           </div>
         </div>
       </div>
