89ec242acb6614eac0eda08548b0dd751655ba91
Jorge Matos <jisapjisap@gmail.com>
2025-09-19 17:00:37 +0200

    Enhance Cars page with search functionality and category filtering. Add input for searching cars and checkboxes for selecting categories, improving user experience and interactivity.

--- src/Pages/Cars.jsx
+++ src/Pages/Cars.jsx
@@ -67,12 +67,43 @@ const Cars = () => {
           </h6>
 
           <h1 className='text-4xl lg:text-5xl xl:text-8xl font-semibold font-bricolage text-red-600'>
-            Select <span>Luxury Cars</span>
+            <span className='text-white font-bricolage'>Select</span> Luxury Car
           </h1>
         </div>
       </div>
 
-      
+      <div className='flex flex-col-reverse md:flex-row gap-8 lg:px-[12%] px-[8%] py-[50px] lg:py-[90px]'>
+        <div className='w-full sticky top-0 md:w-[300px] bg-[#1e1f22] rounded-2xl p-6 shadow-lg animate-side-left h-full'>
+          <div className='mb-6'>
+            <input 
+              type='text' 
+              placeholder='Search cars...'
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              className='w-full px-4 py-2 rounded-lg bg-[#121212] text-white border border-gray-600 outline-none' 
+            />
+          </div>
+
+          <div className='mb-6'>
+            <h4 className='font-semibold font-bricolage text-white text-2xl mb-3'>
+              Categories
+            </h4>
+
+            <ul className='text-md space-y-2 text-gray-400'>
+              {categories.map((cat) => (
+                <li key={cat} className='flex items-center gap-2'>
+                  <input 
+                    type='checkbox' 
+                    checked={selectedCategories.includes(cat)}
+                    onChange={() => handleCheckboxChange(cat, selectedCategories, setSelectedCategories)}
+                  />
+                  <span className='hover:text-white transition-colors duration-300'>{cat}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
